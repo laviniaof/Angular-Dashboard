b4fa661961bb0d69cfd6410f84f2a6dd4b625c98
Lavínia <laviniaof@hotmail.com>
2025-03-21 11:22:39 -0300

    add contentChild decorator

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,4 +1,10 @@
-import { Component, input } from '@angular/core';
+import {
+  Component,
+  ContentChild,
+  ElementRef,
+  inject,
+  input,
+} from '@angular/core';
 
 @Component({
   selector: 'app-control',
@@ -7,10 +13,20 @@ import { Component, input } from '@angular/core';
   templateUrl: './control.component.html',
   styleUrl: './control.component.css',
   host: {
-    class: 'control'
-  }
+    class: 'control',
+  },
 })
 export class ControlComponent {
   /**Instead of use @Input decorator, we are using signal */
   label = input.required<string>();
+  private el = inject(ElementRef);
+  @ContentChild('input') private control?: ElementRef<
+    HTMLInputElement | HTMLAreaElement
+  >;
+
+  OnClick() {
+    console.log('Clicked!');
+    console.log(this.el);
+    console.log(this.control);
+  }
 }
